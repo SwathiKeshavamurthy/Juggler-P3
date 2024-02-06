bbefd615d8db79dd40906ec3dfd430fdad8cd9e5
swathi keshavamurthy <swathik.2197@gmail.com>
2024-02-06 22:17:31 +0000

    Add programs lists to welcome_msg function

--- run.py
+++ run.py
@@ -6,8 +6,11 @@ def welcome_msg():
     print("This is a Juggler multi-program tool.")
     print(
         "Program1-You can convert numbers into words between 1 to Trillion.\n"
-        "Program2-You can type a sentence to count all the characters separately and displayed.\n"
-        "Program3-You should guess a number between 1-10.\n")
+        "Program2-You can get the weather information of the city you request.\n"
+        "Program3-You can find out the Day of Birth.\n"
+        "Program4-You can type a sentence to count all the characters separately and displayed.\n"
+        "Program5-You should guess a number between 1-10.\n")
 
 # Calling the function to display the welcome message
 welcome_msg()
+
