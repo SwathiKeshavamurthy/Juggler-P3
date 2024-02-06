73b65d1874d35dfcd163baf23d084fc379d6c81e
swathi keshavamurthy <swathik.2197@gmail.com>
2024-02-06 21:06:49 +0000

    Add welcome message function

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def welcome_msg():
+    """
+    Print banner msg and intro text
+    """
+    print("Welcome")
+    print("This is a Juggler multi-program tool.")
+    print(
+        "Program1-You can convert numbers into words between 1 to Trillion.\n"
+        "Program2-You can type a sentence to count all the characters separately and displayed.\n"
+        "Program3-You should guess a number between 1-10.\n")
+
+# Calling the function to display the welcome message
+welcome_msg()
